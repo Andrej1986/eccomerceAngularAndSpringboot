5246ef4fa79505f645f6197c26446034338af6d7
soukupa <soukup.andrej@gmail.com>
2022-08-14 19:32:31 +0200

    branch2 final test

--- src/main/java/com/soukupandrej/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/soukupandrej/ecommerce/SpringBootEcommerceApplication.java
@@ -62,4 +62,6 @@ public class SpringBootEcommerceApplication {
         System.out.println("Sending an email message.");
         jmsTemplate.convertAndSend("mailbox2", new Email("info@example.com", "Hello"));
     }
+
+    //branch2 final test
 }
